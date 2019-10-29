b4f00ff31bd1ee144a47393335c90ebea843b715
Andrey Kushch <droni2442@gmail.com>
2019-10-29 18:52:44 +0200

    fixes were added

--- index.js
+++ index.js
@@ -6,18 +6,18 @@
     const resultContainer = document.querySelector('.result__container');
     btn.addEventListener('click', parseString);
     createListOfLetters();
-
     function createListOfLetters() {
-        let arrayOfLetters = Array.from(letters);
         for (let i = 2; i !== 10; i++) {
             let tempArray = [];
-            tempArray.push(arrayOfLetters.shift());
-            tempArray.push(arrayOfLetters.shift());
-            tempArray.push(arrayOfLetters.shift());
-            if(i === 7 || i === 9){
-                tempArray.push(arrayOfLetters.shift());
+            for(let i = 0; i < 3; i++){
+                tempArray.push(letters.slice(0, 1));
+                letters = letters.slice(1, letters.length);
             }
-            let element = new Element(i, tempArray);
+            if(i === 7 || i === 9){
+                tempArray.push(letters.slice(0, 1));
+                letters = letters.slice(1, letters.length);
+            }
+            const element = new Element(i, tempArray);
             numbersArray.push(element);
         }
         function Element(name, keys) {
@@ -32,25 +32,23 @@
             ' ': 1
         })
     }
-
     function parseString() {
-        let letters = Array.from(input.value);
+        const letters =  input.value;
         input.value = '';
         resultContainer.innerHTML = '';
-        letters.map((letter) => {
-            numbersArray.map((item) => {
+        for(let i = 0; i < letters.length; i++){
+            numbersArray.forEach((item) => {
                 for (key in item) {
-                    if (key === letter) {
+                    if (key === letters[i]) {
                         addResult(item.name, item[key]);
                     }
                 }
             })
-        });
+        }
     }
-
     function addResult(number, time){
         const li = document.createElement('li');
         li.innerHTML = `Number ${number} : ${time} times `;
         resultContainer.appendChild(li);
     }
-}());
+ }());
